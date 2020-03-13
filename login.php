ff02bab8c633be02963287d7e06781c8ad019765
deviwulan <62131103+deviwulan@users.noreply.github.com>
2020-03-13 14:31:26 +0700

    File web pengaduan masyarakat
    Tugas ukk membuat website pengaduan

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Pengaduan Masyarakat</title>
+
+<!-- Bootstrap CSS-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/style/style.css">
+</head>
+
+<body>
+    <div class="background">
+<div class="container">
+<div class="row">
+<div class="col-sm-3 col-md-3 col-lg-3"></div>
+
+<div class="col-sm-6 col-md-6 col-lg-6">
+    
+       
+            <div class="form-login">
+                <div class="card white shadow p-3 mb-5 bg-white rounded">
+                    <div class="card-body">
+                        <h1 class="text-center">Login</h1>
+             <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email </label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <small id="emailHelp" class="form-text text-muted">Wajib di isi dengan benar</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+             <button type="submit" class="btn btn-primary">Login</button>
+          </form>
+      </div>
+</div>
+<div class="col-sm-4"></div>
+    </div>
+
+
+
+<!-- Js Bootstrap-->
+ <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
